32137b54103142180857b6b1f8ba22cc0adca548
babalolamukhtar145-collab <babalolamukhtar145@gmail.com>
2026-04-30 08:35:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple function for button click
+function showMessage() {
+  alert("Hello 👋 Your web app is working perfectly!");
+}
+
+// Example: change text on page load
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Website loaded successfully 🚀");
+});
